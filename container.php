e2b1a46ab340ac64d3f9d028230fd3057e79c27a
Back2Basic <78854939+Back2Basic-T@users.noreply.github.com>
2022-06-27 14:07:11 +0800

    Add files via upload

---
+++ container.php
@@ -0,0 +1,12 @@
+</head>
+<body class="">
+<nav role="navigation" class="navbar navbar-dark navbar-static-top bg-primary bg-gradient">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand">JinxHub Food Reservaton</a>
+      </div>
+    </div>
+  </nav>
+	
+	<div class="container" style="min-height:500px;">
+	
